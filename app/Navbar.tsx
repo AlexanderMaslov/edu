4e97d4d1b2156b922d56e67a0f0c2973189b2e07
AlexanderMaslov <maslov-composer@yandex.ru>
2023-11-06 21:31:13 +0300

    Navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,17 +1,38 @@
+'use client';
+
 import React from 'react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { AiFillBug } from 'react-icons/ai';
+import classNames from 'classnames';
 
 const Navbar = () => {
+  const pathname = usePathname();
+
+  const links = [
+    { label: 'Dashboard', href: '/' },
+    { label: 'Issues', href: '/issues' },
+  ];
+
   return (
-    <nav className="flex space-x-6 ">
-      <Link href="/">Logo</Link>
+    <nav className="mb-5 flex items-center space-x-6 border-b px-5 py-4">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
       <ul className="flex space-x-6">
-        <li>
-          <Link href="/">Dashboard</Link>
-        </li>
-        <li>
-          <Link href="/issues">Issues</Link>
-        </li>
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              href={link.href}
+              className={classNames('transition-colors hover:text-zinc-800 ', {
+                'text-zinc-500': link.href !== pathname,
+                'text-zinc-900': link.href === pathname,
+              })}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
