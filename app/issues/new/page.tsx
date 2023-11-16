c90c1b8f1999a396c7dd4b8f6ea13a9b1f4b0279
AlexanderMaslov <maslov-composer@yandex.ru>
2023-11-16 23:22:39 +0300

    handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,45 +1,63 @@
 'use client';
 
-import React from 'react';
-import { TextField, Button } from '@radix-ui/themes';
-import { useForm, Controller } from 'react-hook-form';
-import SimpleMDE from 'react-simplemde-editor';
+import { ErrorMessage } from '@/app/components/ErrorMessage';
+import { Issue, createIssueSchema } from '@/app/validationSchemas';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button, Callout, TextField } from '@radix-ui/themes';
 import 'easymde/dist/easymde.min.css';
 import { useRouter } from 'next/navigation';
-
-interface IssueForm {
-  title: string;
-  description: string;
-}
+import { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import SimpleMDE from 'react-simplemde-editor';
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Issue>({
+    resolver: zodResolver(createIssueSchema),
+  });
+  const [error, setError] = useState('');
 
-  const onSubmit = async (data: IssueForm) => {
-    const res = await fetch('/api/issues', {
-      method: 'POST',
-      body: JSON.stringify(data),
-    });
-    if (!res.ok) throw new Error('failed...');
-    router.push('/issues');
+  const onSubmit = async (data: Issue) => {
+    try {
+      const res = await fetch('/api/issues', {
+        method: 'POST',
+        body: JSON.stringify(data),
+      });
+      if (!res.ok) throw new Error('failed...');
+      router.push('/issues');
+    } catch (error) {
+      setError('An unexpected error occured');
+    }
   };
 
   return (
-    <form className="max-w-xl space-y-3" onSubmit={handleSubmit(onSubmit)}>
-      <TextField.Root>
-        <TextField.Input placeholder="Title" {...register('title')} />
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE {...field} placeholder="Description" />
-        )}
-      />
-
-      <Button>Submit New Issue</Button>
-    </form>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className="space-y-3" onSubmit={handleSubmit(onSubmit)}>
+        <TextField.Root>
+          <TextField.Input placeholder="Title" {...register('title')} />
+        </TextField.Root>
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE {...field} placeholder="Description" />
+          )}
+        />
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
   );
 };
 
