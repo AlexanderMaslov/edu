2ee45434416c44db02900c9a7cd9c394d786076a
AlexanderMaslov <maslov-composer@yandex.ru>
2023-11-19 21:40:41 +0300

    show a confirmation dialog box

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,5 +1,30 @@
-import { Button } from '@radix-ui/themes';
+'use client';
 
-export const DeleteIssueButton = ({}: { issueId: number }) => {
-  return <Button color="red">Delete Issue</Button>;
+import { AlertDialog, Button, Flex } from '@radix-ui/themes';
+
+// eslint-disable-next-line no-unused-vars
+export const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
+  return (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deleteion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure want to delete this issue? This action cannot be undone.
+        </AlertDialog.Description>
+        <Flex mt="4" gap="3">
+          <AlertDialog.Cancel>
+            <Button color="gray" variant="soft">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red">Delete Issue</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
 };
