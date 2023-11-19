4837d31ace893b9e40500e0344541e71bbf3ec71
AlexanderMaslov <maslov-composer@yandex.ru>
2023-11-19 17:17:42 +0300

    add additional loading skeletons

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import { Box } from '@radix-ui/themes';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 const LoadingNewIssuePage = () => {
-  return <>Loading...</>;
+  return (
+    <Box className="max-w-xl">
+      <Skeleton />
+      <Skeleton />
+      <Skeleton height="20 rem" />
+    </Box>
+  );
 };
 
 export default LoadingNewIssuePage;
