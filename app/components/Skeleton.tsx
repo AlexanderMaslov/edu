00fb8c2886c5be30a846b83fa4cdf172cc250d04
AlexanderMaslov <maslov-composer@yandex.ru>
2023-11-19 17:45:35 +0300

    refactor: organize import statements

---
+++ app/components/Skeleton.tsx
@@ -0,0 +1,4 @@
+import SkeletonBase from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+
+export const Skeleton = SkeletonBase;
---
+++ app/components/index.ts
@@ -0,0 +1,5 @@
+export * from './ErrorMessage';
+export * from './IssueStatusBadge';
+export * from './Link';
+export * from './Spinner';
+export * from './Skeleton';
