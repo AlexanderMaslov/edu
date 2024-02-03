945f6dbe757f5a628b34b06bb045852f3a18df56
Alexander <maslov-composer@yandex.ru>
2024-02-04 00:08:46 +0300

    extracting members of a discriminated union

---
+++ src/06-extract-from-discriminated-union.ts
@@ -0,0 +1,6 @@
+export type Event =
+  | { type: "click"; event: MouseEvent }
+  | { type: "focus"; event: FocusEvent }
+  | { type: "keydown"; event: KeyboardEvent };
+
+type ClickEvent = Extract<Event, { type: "click" }>;
