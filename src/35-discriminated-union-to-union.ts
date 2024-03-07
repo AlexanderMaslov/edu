0a8fa1aef05007432d35b91a1a74332a1940fc51
Alexander <maslov-composer@yandex.ru>
2024-03-07 21:58:50 +0300

    iteratively map and remap to transform types

---
+++ src/35-discriminated-union-to-union.ts
@@ -0,0 +1,17 @@
+export type Fruit =
+  | {
+      name: "apple";
+      color: "red";
+    }
+  | {
+      name: "banana";
+      color: "yellow";
+    }
+  | {
+      name: "orange";
+      color: "orange";
+    };
+
+type TransformedFruit = {
+  [K in Fruit as K["name"]]: `${K["name"]}:${K["color"]}`;
+}[Fruit["name"]];
