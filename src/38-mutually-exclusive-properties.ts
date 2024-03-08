e263b1519bca2a72c6748fae4f6396e223903caa
AlexanderMaslov <maslov-composer@yandex.ru>
2024-03-08 10:09:20 +0300

    create a discriminated union throught intermediary transformations

---
+++ src/38-mutually-exclusive-properties.ts
@@ -0,0 +1,11 @@
+interface Attributes {
+  id: string;
+  email: string;
+  username: string;
+}
+
+type MutuallyExclusive<T> = {
+  [K in keyof T]: Record<K, T[K]>;
+}[keyof T];
+
+export type T = MutuallyExclusive<Attributes>;
