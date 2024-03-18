b7241a37b5927a633df1453789980b67a0001a8a
Alexander <maslov-composer@yandex.ru>
2024-03-18 14:17:32 +0300

    refactoring functions with unnecessary type arguments

---
+++ src/generics/20-spotting-useless-generics.ts
@@ -0,0 +1,7 @@
+export const returnBothOfWhatIPassIn = <T extends { a: unknown; b: unknown }>(
+  params: T
+): [T["a"], T["b"]] => {
+  return [params.a, params.b];
+};
+
+const result = returnBothOfWhatIPassIn({ a: "a", b: 1 });
