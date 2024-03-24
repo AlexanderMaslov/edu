b52edd415874b625775a4552951e53dd38d09430
AlexanderMaslov <maslov-composer@yandex.ru>
2024-03-24 16:40:57 +0300

    testing getShippingInfo

--- src/mocking.test.js
+++ src/mocking.test.js
@@ -1,8 +1,10 @@
 import { vi, it, expect, describe } from 'vitest';
-import { getPriceInCurrency } from './mocking';
+import { getPriceInCurrency, getShippingInfo } from './mocking';
 import { getExchangeRate } from './libs/currency';
+import { getShippingQuote } from './libs/shipping';
 
 vi.mock('./libs/currency');
+vi.mock('./libs/shipping');
 
 describe('test suite', () => {
   it('test case', () => {
@@ -21,5 +23,20 @@ describe('getPriceInCurrency', () => {
     getExchangeRate.mockReturnValue(1.5);
 
     expect(getPriceInCurrency(10, 'AUD')).toBe(15);
+  });
+});
+
+describe('getShippingInfo', () => {
+  it('should return shipping unavailable if quote cannot be fetched', () => {
+    getShippingQuote.mockReturnValue(null);
+
+    expect(getShippingInfo('London')).toMatch(/unavailable/i);
+  });
+
+  it('should return shipping info if quote can be fetched', () => {
+    getShippingQuote.mockReturnValue({ cost: 10, estimatedDays: 2 });
+    const result = getShippingInfo('London');
+
+    expect(result).toMatch(/shipping cost: \$10 \(2 days\)/i);
   });
 });
