18e1eaf55e12079f90ea27eb3cfdcf313b4da29f
AlexanderMaslov <maslov-composer@yandex.ru>
2024-03-24 15:01:33 +0300

    testing a stack

--- src/core.test.js
+++ src/core.test.js
@@ -1,4 +1,4 @@
-import { it, expect, describe } from 'vitest';
+import { it, expect, describe, beforeEach } from 'vitest';
 import {
   calculateDiscount,
   canDrive,
@@ -6,6 +6,7 @@ import {
   getCoupons,
   isPriceInRange,
   isValidUsername,
+  Stack,
   validateUserInput,
 } from './core';
 
@@ -162,5 +163,62 @@ describe('fetchData', () => {
       expect(error).toHaveProperty('reason');
       expect(error.reason).toMatch(/fail/i);
     }
+  });
+});
+
+describe('Stack', () => {
+  let stack;
+
+  beforeEach(() => {
+    stack = new Stack();
+  });
+
+  it('push should add an item to the stack', () => {
+    stack.push(1);
+    expect(stack.size()).toBe(1);
+  });
+
+  it('pop should remove and return the top item from the stack', () => {
+    stack.push(1);
+    stack.push(2);
+    expect(stack.pop()).toBe(2);
+    expect(stack.size()).toBe(1);
+  });
+
+  it('pop should throw an error if stack is empty', () => {
+    expect(() => stack.pop()).toThrow(/empty/i);
+  });
+
+  it('peek should return the top item from the stack without removing it', () => {
+    stack.push(1);
+    stack.push(2);
+    expect(stack.peek()).toBe(2);
+    expect(stack.size()).toBe(2);
+  });
+
+  it('peek should throw an error if stack is empty', () => {
+    expect(() => stack.peek()).toThrow(/empty/i);
+  });
+
+  it('isEmpty should return true if stack is empty', () => {
+    expect(stack.isEmpty()).toBe(true);
+  });
+
+  it('isEmpty should false true if stack is not empty', () => {
+    stack.push(1);
+    expect(stack.isEmpty()).toBe(false);
+  });
+
+  it('size should return the number of items in the stack', () => {
+    stack.push(1);
+    stack.push(2);
+    expect(stack.size()).toBe(2);
+  });
+
+  it('clear should remove all items from the stack', () => {
+    stack.push(1);
+    stack.push(2);
+    stack.clear();
+    expect(stack.size()).toBe(0);
   });
 });
