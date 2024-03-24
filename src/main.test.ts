7c9d3f100f353f4f05da7cdf4efc766fe397d66c
AlexanderMaslov <maslov-composer@yandex.ru>
2024-03-24 23:24:46 +0300

    setup

---
+++ src/main.test.ts
@@ -0,0 +1,8 @@
+import { it, expect, describe } from 'vitest';
+import { i } from 'vitest/dist/reporters-P7C2ytIv.js';
+
+describe('group', () => {
+  it('should', () => {
+    expect(1).toBeTruthy();
+  });
+});
