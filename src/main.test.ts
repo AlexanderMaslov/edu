673e84d193ab43285db58d8e3c28845f6aefea7e
AlexanderMaslov <maslov-composer@yandex.ru>
2024-04-14 16:45:35 +0300

    generating fake data

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,9 +1,11 @@
 import { describe, expect, it } from 'vitest';
+import { faker } from '@faker-js/faker';
 
 describe('group', () => {
   it('should', async () => {
-    const response = await fetch('/categories');
-    const data = await response.json();
-    expect(data).toHaveLength(3);
+    console.log({
+      name: faker.commerce.productName(),
+      price: faker.commerce.price({ min: 1, max: 100 }),
+    });
   });
 });
