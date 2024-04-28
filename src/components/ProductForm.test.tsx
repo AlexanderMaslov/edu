647a062ef134fa9e4372d303c826cd493cfe92a2
AlexanderMaslov <maslov-composer@yandex.ru>
2024-04-28 12:42:16 +0300

    testing validation rules

--- src/components/ProductForm.test.tsx
+++ src/components/ProductForm.test.tsx
@@ -62,7 +62,7 @@ describe('ProductForm', () => {
     {
       scenario: 'longer than 255 characters',
       name: 'a'.repeat(256),
-      errorMessage: /255/i,
+      errorMessage: /255/,
     },
   ])(
     'should display an error if name is $scenario',
@@ -78,6 +78,52 @@ describe('ProductForm', () => {
 
       const priceInput = screen.getByPlaceholderText(/price/i);
       await user.type(priceInput, '10');
+
+      const categoryInput = screen.getByRole('combobox', { name: /category/i });
+      await user.click(categoryInput);
+      const options = screen.getAllByRole('option');
+      await user.click(options[0]);
+
+      const submitButton = screen.getByRole('button');
+      await user.click(submitButton);
+
+      const error = screen.getByRole('alert');
+      expect(error).toBeInTheDocument();
+      expect(error).toHaveTextContent(errorMessage);
+    },
+  );
+
+  it.each([
+    { scenario: 'missing', errorMessage: /required/i },
+    {
+      scenario: '0',
+      price: 0,
+      errorMessage: /1/,
+    },
+    {
+      scenario: 'negative',
+      price: -1,
+      errorMessage: /1/,
+    },
+    {
+      scenario: 'greater than 1000',
+      price: 1001,
+      errorMessage: /1000/,
+    },
+  ])(
+    'should display an error if price is $scenario',
+    async ({ price, errorMessage }) => {
+      const user = await userEvent.setup();
+      render(<ProductForm onSubmit={vi.fn()} />);
+      await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
+
+      const nameInput = screen.getByPlaceholderText(/name/i);
+      await user.type(nameInput, 'a');
+
+      if (price !== undefined) {
+        const priceInput = screen.getByPlaceholderText(/price/i);
+        await user.type(priceInput, price.toString());
+      }
 
       const categoryInput = screen.getByRole('combobox', { name: /category/i });
       await user.click(categoryInput);
