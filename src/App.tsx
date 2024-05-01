4b1003542ae4da1ad57007a4e19380a2a67049d5
AlexanderMaslov <maslov-composer@yandex.ru>
2024-05-01 23:44:39 +0300

    devtools

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import store from './store';
 import { bugAdded, bugRemoved, bugResolved } from './actions';
 
 store.dispatch(bugAdded('bug1'));
+store.dispatch(bugAdded('bug2'));
+store.dispatch(bugAdded('bug3'));
 store.dispatch(bugResolved(1));
 
 console.log('store...', JSON.stringify(store.getState(), null, 2));
